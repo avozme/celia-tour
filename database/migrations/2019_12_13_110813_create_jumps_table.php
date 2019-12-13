5487cc2423c7b15ae5f80871700036847c5394b4
Rosendo <rosendodg@gmail.com>
2019-12-13 21:41:20 +0100

    migraciones de las tablas saltos, opciones y visitas guiadas

---
+++ database/migrations/2019_12_13_110813_create_jumps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJumpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jumps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('destination_pitch');
+            $table->integer('destination_yaw');
+            $table->integer('id_scene_destination');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jumps');
+    }
+}
