86ac8d9f9a2c5878ca80561cc6cfac7e2cbbb830
Alejandro <alejandroclares20@gmail.com>
2019-12-13 12:44:12 +0100

    migraciones

---
+++ database/migrations/2019_12_13_094705_create_image_gallery_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImageGalleryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_gallery', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->foreign("id_gallery")->references("id")->on("gallery");
+            $table->foreign("id_resources")->references("id")->on("resources");
+            $table->integer("order");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_gallery');
+    }
+}
---
+++ database/migrations/2019_12_13_094807_create_secondary_scenes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSecondaryScenesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secondary_scenes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string("name");
+            $table->date("date");
+            $table->integer("hfov");
+            $table->integer("pitch");
+            $table->integer("yaw");
+            $table->string("image_file");
+            $table->string("preview_file");
+            $table->foreign("id_scenes")->references("id")->on("scenes");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secondary_scenes');
+    }
+}
