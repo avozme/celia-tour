92236d8c58b52f67bc5a241fcbcc3a882765dc94
Rosendo <rosendodg@gmail.com>
2019-12-17 13:21:41 +0100

    Migración de trabla scenes

---
+++ database/migrations/2019_12_17_121242_create_scenes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScenesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scenes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer(('pitch');
+            $table->integer('yaw');
+            $table->string('directory_name');
+            $table->integer('id_zona');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scenes');
+    }
+}
