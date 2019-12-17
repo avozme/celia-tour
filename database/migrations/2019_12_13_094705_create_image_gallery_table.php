8e1e81581625125bf944a82ccca1993382454570
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2019-12-17 14:37:38 +0100

    Arregladas foreign key

--- database/migrations/2019_12_13_094705_create_image_gallery_table.php
+++ database/migrations/2019_12_13_094705_create_image_gallery_table.php
@@ -15,15 +15,9 @@ class CreateImageGalleryTable extends Migration
     {
         Schema::create('image_gallery', function (Blueprint $table) {
             $table->bigIncrements('id');
-<<<<<<< HEAD
-            $table->timestamps()->useCurrent();
-            $table->integer("id_gallery");
-            $table->integer("id_resources");
-=======
             $table->dateTime('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrent();
             //$table->timestamps()->useCurrent();
->>>>>>> 2019013e4a3363d5d83a1d1e026cfbe7d5732c95
             $table->foreign("id_gallery")->references("id")->on("gallery");
             $table->foreign("id_resources")->references("id")->on("resources");
             $table->integer("order");
