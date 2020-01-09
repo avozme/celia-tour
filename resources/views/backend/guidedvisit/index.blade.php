841c3d363eb78318c9bc41e8104ca82c5962bf22
Alejandro <alejandroclares20@gmail.com>
2020-01-09 13:05:07 +0100

    modified:   resources/views/backend/guidedvisit/index.blade.php 	modified:   routes/web.php

--- resources/views/backend/guidedvisit/index.blade.php
+++ resources/views/backend/guidedvisit/index.blade.php
@@ -20,11 +20,36 @@
                 <td>{{$value->description}}</td>
                 <td>{{$value->file_preview}}</td>
                 <td><a href="{{ route('guidedVisit.edit', $value->id) }}">Modificar</a></td>
-                <td>Eliminar</td>
+                <td><span class="delete" id="{{$value->id}}">Eliminar<span></td>
             </tr>
             @endforeach
             
         </tbody>
     </table>
-
+    <script>
+        $(function() {
+            $("#delete").click(function(){
+            var isDelte = confirm("¿Esta seguro que desea eliminar la visita guiada?");
+            if(isDelte){
+                var domElement = $(this);
+                var id = $(this).attr("id");
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function(){
+                    if(this.readyState == 4 && this.status == 200){ 
+                        if (xhttp.responseText == 1) {
+                            $(domElement.parent()).fadeOut(500, function(){
+                                $(domElement).parent().remove();
+                            });
+                        } else {
+                            alert("Algo fallo!");
+                        }
+                    }
+                }
+                var direccion = "http://localhost:3000/guidedVisit/delete/"+id;
+                xhttp.open("GET", direccion, true);
+                xhttp.send();
+            }
+            });
+        });
+    </script>    
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,13 @@ Route::get('/', function () {
 
 /////////////// RESTfull Visitas Guiadas ////////////////
 Route::resource('guidedVisit', 'GuidedVisitController');
+Route::get('movie/delete/{id}', 'GuidedVisitController@destroy')->name('guidedVisit.delete');
 
 /////////////// RESTfull Recuersos ////////////////
 Route::resource('resources', 'ResourcesController');
-Route::get('resources/{id}/delete', 'ResourcesController@destroy')->name('zone.delete');
-Route::get('resources/{id}/edit', 'ResourcesController@edit')->name('zone.edit');
-Route::post('resources/{id}', 'ResourcesController@update')->name('zone.update');
+Route::get('resources/{id}/delete', 'ResourcesController@destroy')->name('resources.delete');
+Route::get('resources/{id}/edit', 'ResourcesController@edit')->name('resources.edit');
+Route::post('resources/{id}', 'ResourcesController@update')->name('resources.update');
 
 /////////////// RESTfull Zonas ////////////////
 Route::get('zone/{id}/delete', 'ZoneController@destroy')->name('zone.delete');
