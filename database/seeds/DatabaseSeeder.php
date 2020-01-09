cdabf298dce3c99458c64c0e903a794682a2c69d
Jose-bousfanj-95 <jose.bousfanj.m@gmail.com>
2020-01-09 13:19:52 +0100

    vista de options

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(ResourceTableSeeder::class);
+        $this->call(OptionsTableSeeder::class);
     }
 }
