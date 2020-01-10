41fcc6a3db363e58bd67390b9106d8ea7b94aa2f
mmarbonillo <mbonillo95@gmail.com>
2020-01-10 11:13:41 +0100

    Modificado controlador y vista create de zonas para el nuevo campo de la tabla

--- resources/views/backend/zone/create.blade.php
+++ resources/views/backend/zone/create.blade.php
@@ -14,6 +14,8 @@
             <input type="file" name="file_miniature"><br><br>
             <label for="position">Position</label>
             <input type="number" name="position"><br><br>
+            <input type="checkbox" name="initial_zone">
+            <label for="initial_zone">Selecciona esta opción si quieres que tu visita libre empiece en esta zona.</label><br><br>
             <input type="submit" value="Añadir">
         </form>
     </div>
