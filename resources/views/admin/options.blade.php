506e3910b0076d59e55c764990d1995a11eb9007
Jose-bousfanj-95 <jose.bousfanj.m@gmail.com>
2020-01-13 09:05:53 +0100

    plugins para editor de texto en la vista Options

--- resources/views/admin/options.blade.php
+++ resources/views/admin/options.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.backend')
-<script>
-function init(x) {
-if (x=="h") {document.getElementById('txtbox').style.display='none';document.getElementById('text').style.display='block';}
-if (x=="s") {document.getElementById('txtbox').style.display='block';document.getElementById('text').style.display='none';}
-document.getElementById('text').value=document.getElementById('txtbox').innerHTML;
-if (x!="h" || x!="s") document.execCommand(x,false,null);
-document.getElementById('txtbox').focus();
-}
-</script>
+
+<script src="{{ asset('/vendors/ckeditor/ckeditor.js') }}"></script>
 @section('content')
     <h2>Aquí van los datos de la tabla options</h2>
         
@@ -20,35 +13,33 @@ document.getElementById('txtbox').focus();
                 {{$op->key}}: <input type="file" name="option" value="{{$op->value ?? '' }}"> <input type="submit" value="Editar"></td><br>
                 <img src='{{ url('img/options/'.$op->value) }}' alt='options' height='250px' width='250px'>
             @elseif (strpos($op->value, 'cripcion')==true)
-                <input type="hidden" name="option" value="{{$op->value ?? '' }}"> </td>
-                <p> 
-                <button onclick="init('bold')">Negrita</button>
-                <button onclick="init('italic')">Itálica</button>
-                <button onclick="init('underline')">Subrayado</button>
-                <button onclick="init('justifycenter')">Centrado</button>
-                <button onclick="init('increasefontsize')">Fuente +</button>
-                <button onclick="init('inserthorizontalrule')">Línea Hr.</button>
-                <button onclick="init('redo')">Rehacer</button>
-                <button onclick="init('undo')">Deshacer</button>
-                <button onclick="init('s')">Vista Real</button>
-                <button onclick="init('h')">Vista HTML</button>
-                </p>
-                <div id="txtbox" contenteditable="true">
-                <h2>{{$op->value}}</h2>
-                <p>Escribe aquí ...</p>
-                <p>Etc ...</p>
+                <input type="hidden" name="option" value="{{$op->value ?? '' }}">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">{{$op->key}}</div>
+             
+                                <div class="panel-body">
+                                    <form>
+                                        <textarea class="ckeditor" name="editor1" id="editor1" rows="10" cols="80">
+                                            Este es el textarea que es modificado por la clase ckeditor
+                                        </textarea>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <textarea id="text" name="text"></textarea>
             @else          
                 {{$op->key}}: <input type="text" name="option" value="{{$op->value ?? '' }}"> <input type="submit" value="Editar"></td>
                 
             @endif   
             </form> 
 
+
                      
             @endforeach
-            
-            
             
 
 @endsection
