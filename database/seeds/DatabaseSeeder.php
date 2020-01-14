08d09a39df170364a45ddfe99897a3c43f6dabf8
Alejandro <alejandroclares20@gmail.com>
2020-01-14 13:02:10 +0100

    Seed actualizada

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        //$this->call(GuidedVisitTableSeeder::class);
+        $this->call(GuidedVisitTableSeeder::class);
         $this->call(ResourceTableSeeder::class);
         $this->call(OptionsTableSeeder::class);
     }
