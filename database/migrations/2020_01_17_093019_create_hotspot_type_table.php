7f5aa3673fa309793aeb5d30a9de8e3c3f4670b5
Angel Salas Calvo <angelsalascalvo@gmail.com>
2020-01-17 15:15:50 +0100

    Creado controlador, modelo y migracion para la tabla intermedia entre hotspot y diferentes recursos (HotspotTypes)

---
+++ database/migrations/2020_01_17_093019_create_hotspot_type_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotspotTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotspot_type', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->bigInteger('id_type');
+            $table->integer("type");
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotspot_type');
+    }
+}
