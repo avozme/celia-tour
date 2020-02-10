11b332e6dbe20b9b9137d116b66c16f846d05277
Jose-bousfanj-95 <jose.bousfanj.m@gmail.com>
2020-02-11 00:18:37 +0100

    relacion muchos a muchos de portkey y escenas

--- app/Portkey.php
+++ app/Portkey.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Portkey extends Model
 {
     protected $fillable=['name'];
+
+    public function scene(){
+        return $this->belongsToMany("App\Scene", 'portkeys_scene', 'portkey_id', 'scene_id');
+    }
 }
+
