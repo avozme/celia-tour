cca7e837a57583b09626a3af087c1a21e13d4255
Jose-bousfanj-95 <jose.bousfanj.m@gmail.com>
2020-02-12 10:20:38 +0100

    cambios en database

---
+++ database/migrations/2020_02_11_092259_create_portkey_scene_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortkeySceneTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portkey_scene', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer("portkey_id");
+            $table->integer("scene_id");
+            $table->integer("position");
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portkey_scene');
+    }
+}
