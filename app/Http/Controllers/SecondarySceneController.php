41bdb1f0a200d628e899ff1233dcb152b13cfc5d
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2020-02-12 12:21:26 +0100

    Creadas las funciones del controlador de Escenas Secundarias

--- app/Http/Controllers/SecondarySceneController.php
+++ app/Http/Controllers/SecondarySceneController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SecondaryScene;
 
 class SecondarySceneController extends Controller
 {
@@ -26,7 +27,13 @@ class SecondarySceneController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $s_scene->fill($request->all());
+        $s_scene->save();
+        return Response::json([
+            'id' => $s_scene->id,
+            'name' => $s_scene->name,
+            'date' => $s_scene->date
+        ], 200);
     }
 
     /**
@@ -37,7 +44,13 @@ class SecondarySceneController extends Controller
      */
     public function show($id_scene)
     {
-        
+        $s_scene = SecondaryScene::where('id_scenes', $id_scene)->get();
+        //dd($s_scene);
+        return Response::json([
+            'id' => $s_scene->id,
+            'name' => $s_scene->name,
+            'date' => $s_scene->date
+        ], 200);
     }
 
     /**
@@ -60,7 +73,13 @@ class SecondarySceneController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $s_scene = SecondaryScene::find($id);
+        $s_scene->fill($request->all());
+        if( $s_scene->save()){
+            return response()->json(['status'=> true]);
+        }else{
+            return response()->json(['status'=> false]);
+        }
     }
 
     /**
@@ -71,6 +90,7 @@ class SecondarySceneController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $s_scene = SecondaryScene::find($id);
+        $s_scene->destroy();
     }
 }
