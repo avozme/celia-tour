7907146a0107028f057e71f4d1fe26130cffa194
Angel Salas Calvo <angelsalascalvo@gmail.com>
2020-02-14 16:24:01 +0100

    Progreso en visita libre

--- public/js/hotspot/jump.js
+++ public/js/hotspot/jump.js
@@ -31,9 +31,8 @@ function jump(id, title, description, pitch, yaw){
     //AGREGAR HTML DEL HOTSPOT
     var urljumpimage = $('#urljump').val();
     $("#contentHotSpot").append(
-        "<div id='hintspot' class='jump hots"+ id +" hint--right hint--info hint--bounce' data-hint='hint.css!' jumpid='"+ id +"' >"+
-                "<img src='"+ urljumpimage +"' >" +
-                "</a>" +
+        "<div id='hintspot' class='jump hots"+ id +"' jumpid='"+ id +"' >"+
+            "<svg  xmlns='http://www.w3.org/2000/svg' viewBox='0 0 250.1 127.22'><path d='M148.25,620.61l1.15-.79q61.83-39.57,123.65-79.15a1.71,1.71,0,0,1,2.2,0Q336,580.08,396.72,619.44l1.63,1.11a8,8,0,0,0-1.18.74l-46.73,45.15c-1.4,1.36-1.41,1.36-3,.15q-36.37-27.75-72.71-55.53a1.78,1.78,0,0,0-2.62,0q-37.26,28-74.56,55.86c-.85.64-1.37.72-2.2-.09q-23.1-22.68-46.24-45.32C148.84,621.25,148.58,621,148.25,620.61Z' transform='translate(-148.25 -540.26)' fill='white'/></svg>"+
         "</div>"
         );
     jumpId = id;
