fd9dfdb29b353d610f60304c09ac3a8c3098d38f
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2020-02-18 12:17:44 +0100

    Terminada la vista previa al modificar escena secundaria

--- resources/views/backend/zone/edit.blade.php
+++ resources/views/backend/zone/edit.blade.php
@@ -124,7 +124,7 @@
            <polygon points="28,22.398 19.594,14 28,5.602 22.398,0 14,8.402 5.598,0 0,5.602 8.398,14 0,22.398 5.598,28 14,19.598 22.398,28"/>
        </svg>
     </button>
-    <div class="addVideoContent col100 xlMarginTop">
+    <div class="addVideoContent col45 xlMarginTop">
         <form id="updateSceneS" method="post" enctype="multipart/form-data" action="{{ route('sscenes.update') }}">
             @csrf
             <input type="hidden" name="_method" value="POST">
@@ -138,10 +138,11 @@
             <input type="hidden" name="idZone" id="idZone" value="{{$zone->id ?? ''}}">
             <input type="submit" value="Guardar" id="addSScene">
         </form>
-        <div id="sSceneView" style="width: 250px; height: 250px; position: relative">
-            <div id="pano" class="l1 col100" style="position: relative"></div>
-        </div>
     </div>
+    <div class="col10 row50" ></div>
+        <div id="sSceneView" class="col45">
+            <div id="pano" class="l1 col100" style="width: 100%; heigth: 100%"></div>
+        </div>
 </div>   
 @endsection
 
