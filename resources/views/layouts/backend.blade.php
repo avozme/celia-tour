d088285c6fc656dff48b302df3fc0743068e0a3a
mmarbonillo <mbonillo95@gmail.com>
2020-02-25 13:03:36 +0100

    Modificación de rutas ajax de http a https. Modificación del archivo AddGitInformation

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -3,7 +3,7 @@
     <head>
         <link rel="stylesheet" href="{{url('css/global.css')}}" />
         <link rel="stylesheet" href="{{url('css/backend.css')}}" />
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700&display=swap" rel="stylesheet">
         @yield('headExtension')
         <!-- Por defecto title Celia Tour -->
