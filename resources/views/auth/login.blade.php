ff31c9d00b9c56554d1a1ba801965fec3913138d
Angel Salas Calvo <angelsalascalvo@gmail.com>
2020-03-08 14:25:27 +0100

    Cambiado estilo para la vista de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,50 @@
-@extends('layouts.app')
+@extends('layouts.access')
 
 @section('content')
 
-<div id="title" class="col80"><h1>Login</h1></div>
+<div id="content" class="col100 centerV row100">
 
-<div id="content" class="col100">
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div class="col100">
-            <label for="email" class="">{{ __('Email') }}</label>
-
-            <div class="">
-                <input id="email" type="text" class="" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                @error('email')
-                    <span class="" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+    <div class="col100" style="margin-top:-5%">
+        <div class="col100 centerH">
+            <img class="col25" src="{{ url('/img/logo.png')}}"/>
         </div>
 
-        <div class="col100">
-            <label for="password" class="">{{ __('Contraseña') }}</label>
+        <form method="POST" action="{{ route('login') }}" class="col100 centerH xlMarginTop">
+            @csrf
+            <div class="col30">
+                <div class="col100">
+                    <div class="">
+                        <input id="email" type="text" class="col100" name="email" placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>                       
+                    </div>
+                </div>
 
-            <div class="">
-                <input id="password" type="password" class="" name="password" required autocomplete="current-password">
+                <div class="col100">
+                    <div class="">
+                        <input id="password" type="password" class="col100 sMarginTop" name="password" placeholder="Contraseña" required autocomplete="current-password">
 
-                @error('password')
-                    <span class="" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+                        @error('password')
+                            <span class="msmError sMarginTop col100" role="alert">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                        @error('email')
+                            <span class="msmError sMarginTop col100" role="alert">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col100">
+                    <div class="">
+                        <button type="submit" class="col100 lMarginTop" >
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div class="col100">
-            <div class="">
-                <button type="submit" class="" >
-                    {{ __('Login') }}
-                </button>
-            </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
             
 @endsection
