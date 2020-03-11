cb0bc43e523f784689d2be783170bf84d6d32f15
erredege <34535136+erredege@users.noreply.github.com>
2020-03-11 12:51:35 +0100

    modifico vista de destacados para que se cambie la modifique la posicion al borrar un punto intermedio

--- app/Http/Controllers/HighlightController.php
+++ app/Http/Controllers/HighlightController.php
@@ -134,7 +134,15 @@ class HighlightController extends Controller{
     public function destroy($id){
         $highlights = Highlight::find($id);
         $file = $highlights->scene_file;
-        unlink(public_path().'/img/resources/'.$file);
+        //unlink(public_path().'/img/resources/'.$file);
+        $allHg = Highlight::all();
+        $position = $highlights->position;
+        for($i = 0; $i < count($allHg); $i++){
+            if($allHg[$i]->position > $position){
+                $allHg[$i]->position -= 1;
+                $allHg[$i]->save();
+            }
+        }
         $highlights->delete();
         return redirect()->route('highlight.index');
     }
