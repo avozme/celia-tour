644819e4c78955164af5a193630ec322746dcef8
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2020-03-24 10:23:40 +0100

    Cambiando el front para que pille el tipo de portada

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -66,62 +66,81 @@
                 $("#txtOption span").removeClass("showTextOption");
             });
         });
-        ///////////////////////////////////////////////////////////////////////////
-        ///////////////////////////   MARZIPANO   /////////////////////////////////
-        ///////////////////////////////////////////////////////////////////////////
-        'use strict';
-        //1. VISOR DE IMAGENES
-        var panoElement = document.getElementById('pano');
-        /* Progresive controla que los niveles de resolución se cargan en orden, de menor 
-        a mayor, para conseguir una carga mas fluida. */
-        var viewer =  new Marzipano.Viewer(panoElement, {stage: {progressive: true}}); 
 
-        //2. RECURSO
-        var source = Marzipano.ImageUrlSource.fromString(
-        "{{url('/marzipano/tiles/'.$data[0]->directory_name.'/{z}/{f}/{y}/{x}.jpg')}}",
-        
-        //Establecer imagen de previsualizacion para optimizar su carga 
-        //(bdflru para establecer el orden de la capas de la imagen de preview)
-        {cubeMapPreviewUrl: "{{url('/marzipano/tiles/'.$data[0]->directory_name.'/preview.jpg')}}", 
-        cubeMapPreviewFaceOrder: 'lfrbud'});
+        var tipoPortada = @json($tipoPortada);
+        var portada = @json($portada);
+        var imagen = "{{url('img/options/image')}}";
+        var url = "{{url('img/options/image')}}";
+        var imagen = url.replace('image', portada[0].value);
 
-        //3. GEOMETRIA 
-        var geometry = new Marzipano.CubeGeometry([
-        { tileSize: 256, size: 256, fallbackOnly: true  },
-        { tileSize: 512, size: 512 },
-        { tileSize: 512, size: 1024 },
-        { tileSize: 512, size: 2048},
-        ]);
-
-        //4. VISTA
-        //Limitadores de zoom min y max para vista vertical y horizontal
-        var limiter = Marzipano.util.compose(
-            Marzipano.RectilinearView.limit.vfov(0.698131111111111, 2.09439333333333),
-            Marzipano.RectilinearView.limit.hfov(0.698131111111111, 2.09439333333333)
-        );
-        //Establecer estado inicial de la vista con el primer parametro
-        var view = new Marzipano.RectilinearView({yaw: "{{$data[0]->yaw}}", pitch: "{{$data[0]->pitch}}", roll: 0, fov: Math.PI}, limiter);
-
-        //5. ESCENA SOBRE EL VISOR
-        var scene = viewer.createScene({
-            source: source,
-            geometry: geometry,
-            view: view,
-            pinFirstLevel: true
-        });
-
-        //6.MOSTAR
-        scene.switchTo({ transitionDuration: 1000 });
-
-        //7. AUTOROTACION
-        var autorotate = Marzipano.autorotate({
-            yawSpeed: 0.03,         // Yaw rotation speed
-            targetPitch: 0,        // Pitch value to converge to
-            targetFov: Math.PI/2   // Fov value to converge to
-        });
-        // Movimiento infinito
-        viewer.setIdleMovement(Infinity);
-        // Empezar rotacion
-        viewer.startMovement(autorotate); 
+        if(tipoPortada[0].value=="Estatica"){
+            console.log(tipoPortada);
+            console.log("estatica");
+            console.log(imagen);
+             $('body').css('background-image', imagen);
+        }else{
+            console.log(tipoPortada);
+            console.log("nanai");
+        }
     </script>
 @endsection
+{{-- 
+else{
+            ///////////////////////////////////////////////////////////////////////////
+            ///////////////////////////   MARZIPANO   /////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////
+            'use strict';
+            //1. VISOR DE IMAGENES
+            var panoElement = document.getElementById('pano');
+            /* Progresive controla que los niveles de resolución se cargan en orden, de menor 
+            a mayor, para conseguir una carga mas fluida. */
+            var viewer =  new Marzipano.Viewer(panoElement, {stage: {progressive: true}}); 
+
+            //2. RECURSO
+            var source = Marzipano.ImageUrlSource.fromString(
+            "{{url('/marzipano/tiles/'.$data[0]->directory_name.'/{z}/{f}/{y}/{x}.jpg')}}",
+            
+            //Establecer imagen de previsualizacion para optimizar su carga 
+            //(bdflru para establecer el orden de la capas de la imagen de preview)
+            {cubeMapPreviewUrl: "{{url('/marzipano/tiles/'.$data[0]->directory_name.'/preview.jpg')}}", 
+            cubeMapPreviewFaceOrder: 'lfrbud'});
+
+            //3. GEOMETRIA 
+            var geometry = new Marzipano.CubeGeometry([
+            { tileSize: 256, size: 256, fallbackOnly: true  },
+            { tileSize: 512, size: 512 },
+            { tileSize: 512, size: 1024 },
+            { tileSize: 512, size: 2048},
+            ]);
+
+            //4. VISTA
+            //Limitadores de zoom min y max para vista vertical y horizontal
+            var limiter = Marzipano.util.compose(
+                Marzipano.RectilinearView.limit.vfov(0.698131111111111, 2.09439333333333),
+                Marzipano.RectilinearView.limit.hfov(0.698131111111111, 2.09439333333333)
+            );
+            //Establecer estado inicial de la vista con el primer parametro
+            var view = new Marzipano.RectilinearView({yaw: "{{$data[0]->yaw}}", pitch: "{{$data[0]->pitch}}", roll: 0, fov: Math.PI}, limiter);
+
+            //5. ESCENA SOBRE EL VISOR
+            var scene = viewer.createScene({
+                source: source,
+                geometry: geometry,
+                view: view,
+                pinFirstLevel: true
+            });
+
+            //6.MOSTAR
+            scene.switchTo({ transitionDuration: 1000 });
+
+            //7. AUTOROTACION
+            var autorotate = Marzipano.autorotate({
+                yawSpeed: 0.03,         // Yaw rotation speed
+                targetPitch: 0,        // Pitch value to converge to
+                targetFov: Math.PI/2   // Fov value to converge to
+            });
+            // Movimiento infinito
+            viewer.setIdleMovement(Infinity);
+            // Empezar rotacion
+            viewer.startMovement(autorotate); 
+        } --}}
