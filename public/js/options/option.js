205ed508164843b827253e3e7bf342df3e5d346d
mmarbonillo <mbonillo95@gmail.com>
2020-03-25 14:13:22 +0100

    Trabajo en issue 81: Añadir ventana modal para previsualizar portada panorámica. Marcar en el mapa la escena actual si es que la hubiese. Añadido cerrar ventanas modales clicando fuera de ellas. Extracción de css y parte del javascript a archivos externos. Editar zonas: Añadida comprobación de formulario de editar escena mediante javascript.

---
+++ public/js/options/option.js
@@ -0,0 +1,63 @@
+$( document ).ready(function() {
+    //Función para el botón cerrar de la modal
+    $(".closeModal").click(function(){
+        console.log("hago click en el cerrar ventana");
+        $("#img-port").empty();
+        $("#modalWindow").css("display", "none");
+        $(".window").css("display", "none");
+    });
+
+    //CAMBIAR APARIENCIA DE LOS PUNTOS DEL MAPA AL ESTAR SOBRE ELLOS
+    $(".scenepoint").hover(function(){
+        $(this).attr('src', urlPointSceneHover);
+    }, function(){
+        if(!($(this).hasClass('selected'))){
+            $(this).attr('src', urlPointScene);
+        }
+    });
+
+    $(".scenepoint").click(function(){
+        //Le quito la clase selected a todos los puntos para que la url de la imagen sea la de la imagen blanca
+        $('.scenepoint').removeClass('selected');
+        $('.scenepoint').attr('src', urlPointScene)
+        //Se la añado al punto seleccionado para que se marque como tal
+        $(this).addClass('selected');
+        $(this).attr('src', urlPointSceneHover);
+        var idScene = ($(this).attr('id')).substr(5);
+        $("#idScene").val(idScene);
+        $('#optionIdScene').val(idScene)
+    });
+    //Funcionalidad del botón aceptar de la modal
+    $("#addPanoramica").click(function(){
+        $("#img-port").empty();
+        $("#modalWindow").css("display", "none");
+        $("#modalMap").css("display", "none");
+        $("#mapSlide").css("display", "none"); 
+    });
+
+    //ONCLICK DE PREVISUALIZAR ESCENA
+    $('#scenePreview').click(function(){
+        var sceneId = $('#optionIdScene').attr('value');
+        loadSceneIfExist(sceneId);
+        $('#pano').css('overflow', 'visible');
+        $('#previewModal').css('display', 'block');
+        $('#modalWindow').show();
+    });
+
+    //CÓDIGO PARA QUE LAS MODALES SE CIERREN AL PINCHAR FUERA DE ELLAS
+    var dentro = false;
+    $('.window').on({
+        mouseenter: function(){
+            dentro = true;
+        },
+        mouseleave: function(){
+            dentro = false;
+        }
+    });
+    $('#modalWindow').click(function(){
+        if(!dentro){
+            $('#modalWindow, .window').hide();
+        }
+    });
+
+});
