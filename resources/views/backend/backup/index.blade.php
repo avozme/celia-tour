94a5c27262b3470c58403b47eb99dfcfea5dda24
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2020-03-28 11:45:07 +0100

    Trabajando en el issue #95

--- resources/views/backend/backup/index.blade.php
+++ resources/views/backend/backup/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.backend')
+<script src="{{url('js/backup/backup.js')}}" ></script>  
 <style>
 #modalWindow .window {
    margin-left: 30%;
@@ -68,56 +69,4 @@ button#aceptDelete{
          <input type="submit" name="cancel" value="Restaurar copia">
       </form>
    </div>
-
-   <script>
-      $(document).ready(function(){
-         //Accion para examinar imagen de restauracion al hacer clic sobre el elemento HTML
-         $('#upBackup').on("click", function(){
-            $('#fileInput').trigger('click');
-         });
-         //Enviar formulario al seleccionar un elemento
-         $("#formRestore").on("change", function(){
-            $("#modalWindow").css("display", "block");
-            $("#containerModal").css("display", "block");
-            $("#confirmDelete").css("display", "block");
-            $("#aceptDelete").click(function(){
-               console.log("enviando");
-               $("#formRestore").submit();
-            });
-            $("#cancelDelete").click(function(){
-               $("#modalWindow").css("display", "none");
-               $("#containerModal").css("display", 'none');
-               $("#confirmDelete").css("display", "none");
-            }); 
-            $("#closeModalWindowButton").click(function(){
-               $("#modalWindow").css("display", "none");
-               $("#containerModal").css("display", 'none');
-               $("#confirmDelete").css("display", "none");
-            });
-         });
-      });
-
-      $("#downBackup").click(function(){
-         console.log("click");
-         $("#modalWindow").css("display", "block");
-         $("#containerModal").css("display", "block");
-         $("#confirmDelete").css("display", "block");
-            $("#aceptDelete").click(function(){
-               $("#modalWindow").css("display", "none");
-               $("#containerModal").css("display", 'none');
-               $("#confirmDelete").css("display", "none");
-               window.location.href="{{route('backup.create')}}"
-            });
-            $("#cancelDelete").click(function(){
-               $("#modalWindow").css("display", "none");
-               $("#containerModal").css("display", 'none');
-               $("#confirmDelete").css("display", "none");
-            }) 
-         $("#closeModalWindowButton").click(function(){
-            $("#modalWindow").css("display", "none");
-            $("#containerModal").css("display", 'none');
-            $("#confirmDelete").css("display", "none");
-         });   
-      });
-   </script>
 @endsection
