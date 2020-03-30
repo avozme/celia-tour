07b9535d49ffca2f871f9aa5ee6c3e24eb80562b
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2020-03-30 11:05:10 +0200

    Renderizando código y sacando código CSS y JS de las vistas de Galerias #30 #95

--- public/js/resources/resources.js
+++ public/js/resources/resources.js
@@ -19,36 +19,6 @@ $().ready(function(){
         $("#video").css("display", "block");
     });
 
-    //FUNCIÓN AJAX PARA BORRAR
-    $(".delete").click(function () {
-        $("#edit").css("display", "none");
-        $("#confirmDelete").css("display", "block");
-        $("#aceptDelete").click(function () {
-            $("#confirmDelete").css("display", "none");
-            $("#modalWindow").css("display", "none");
-            var route = "{{ route('resource.delete', 'req_id') }}".replace('req_id', id);
-            $.ajax({
-                url: route,
-                type: 'POST',
-                data: {
-                    _token: "{{ csrf_token() }}",
-                }, success: function (result) {
-                    if (result.status == true) {
-                        $(elementoD).remove();
-                        $('.previewResource').empty();
-                    } else {
-                        alert("Este recurso no puede ser eliminado por que esta siendo usado en una galeria");
-                        $('.previewResource').empty();
-                    }
-                }
-            });
-        });
-        $("#cancelDelete").click(function () {
-            $("#confirmDelete").css("display", "none");
-            $("#edit").css("display", "block");
-        });
-    });
-
     //Boton subir ficheros
     $("#fileSubtOwn").on("click", function () {
         $("#fileSubt").click();
--- resources/views/backend/gallery/resourceUpdate.blade.php
+++ resources/views/backend/gallery/resourceUpdate.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.backend')
 @section('content')
+@section('headExtension')
+<!--SCRIPT -->
+<script src="{{url('js/gallery/galleryResources.js')}}" ></script> 
+@endsection
+<!--BUSCADOR-->
 <div id="b" class="col45 xlMarginBottom ">
     <form id="buscador" action="{{route('gallery.edit_resources', $gallery->id)}}" method="GET">
         <input class="search_input" type="text" name="texto" placeholder="Search..." onchange="formulario()" >
@@ -14,12 +19,16 @@
     </form>
     @endif
 </div>
+<!--BOTÓN DE GUARDAR-->
 <div  class="col25 xlMarginBottom ">
     <button class="update" onclick="window.location.href='{{route('gallery.index')}}'">Guardar</button>
 </div>
+<!--RECURSOS-->
 <div class="col100" id="container">
     <form method="POST" action="/gallery/{{ $gallery->id ?? ''}}/update_resources" enctype="multipart/form-data">
          @csrf
+         <!--Comprobamos si los recursos estan en la lista de los recursos que pertenecen a una galeria, si lo estan si lo pone el atributo checked, 
+         sino se dejan sin el atributo checked-->
         @foreach ($resources as $r )
             @php
                 $estaEnLista = false
@@ -46,7 +55,7 @@
                 <div class="insideElement relative">
                 <!-- MINIATURA -->
                 <input type="checkbox" name="resources[]" value="{{$r->id}}" class="seleccionado l3 absolute" style="bottom:0; left:0;">
-                <div class="preview col100 l2"><img class="l2"src="{{url('img/resources/'.$r->route)}}" weigth="100px" height="100px"></i></div>
+                <div class="preview col100 l2"><img class="l2"src="{{url('img/resources/miniatures/'.$r->route)}}" weigth="100px" height="100px"></i></div>
                 </div>
             </div>
             @endif    
@@ -55,32 +64,7 @@
     </form>
 </div>
 <script>
-    function formulario(){
-        var valorAction = $("#buscador").attr('action');
-        var texto = $(".search_input").val();
-        $("#buscador").attr('action', valorAction+'/'+texto);
-    }
-
-    $(".seleccionado").click(function(){
-        elemento = $(this).attr("value");
-        idGaleria= $(".idgaleria").attr('id');
-        var url = "{{url('')}}";
-        if( $(this).prop("checked")){
-            estado="true";
-        }else{
-            estado="false";
-        }
-        console.log(estado);
-        console.log(idGaleria);
-        if(estado=="true"){
-            $.get(url+'/gallery/save_resource/'+idGaleria+'/'+elemento, function(respuesta){
-            console.log("entre pòr el if");
-        }); 
-        }else{
-            $.get(url+'/gallery/delete_resource/'+idGaleria+'/'+elemento, function(respuesta){
-            console.log("entre pòr el else");
-        });
-        }
-    });
+    /*Dato necesario para guardar y eliminar recursos de una geleria.*/
+    var url = "{{url('')}}";
 </script>
 @endsection
