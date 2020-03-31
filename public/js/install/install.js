f444a51d2a263abb205872976c406ec56794306b
mmarbonillo <mbonillo95@gmail.com>
2020-03-31 17:34:07 +0200

    Hago commit para que me deje hacer pull ya que jose y yo estamos escribiendo el install juntos

---
+++ public/js/install/install.js
@@ -0,0 +1,50 @@
+$().ready(function(){
+    //COMPROBACIÓN DEL FORMULARIO DE INSTALL EN LA PARTE DE USUARIO ANTES DEL SUBMIT
+    $('#installForm').submit(function(event){
+        var name = document.getElementById('userName').value;
+        var pass1 = document.getElementById("userPass1").value;
+        var pass2 = document.getElementById("userPass2").value;
+        
+        
+        //Comprobamos que todos los campos estén rellenos
+        if(name != "" && pass1 != "" && pass2 != ""){
+            var test = (/^[A-Za-z0-9Ññ]+$/.test(name));
+            //Si el nombre de usuario no cumple los requisitos
+            if(!test){
+                event.preventDefault();
+                $('#errorMsgUser > span').text('El nombre de usuario solo puede contener mayúsculas, minúsculas y números');
+                $('#userName').css('border', '1.5px solid red');
+            }else{
+                $('#userName').css('border', '1px solid black');
+                //Comprobamos que la contraseña cumpla los requisitos mínimos
+                if(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&()])[A-Za-z\d@$!%*#?&]{8,}$/.test(pass1)){
+                    //Comprobamos que las contraseñas coincidan
+                    if(pass1 != pass2){
+                        //Si no coinciden, detenemos el evento submit
+                        event.preventDefault();
+                        $('#errorMsgUser > span').text('Las contraseñas no coinciden');
+                        $('#userPass1').css('border', '1.5px solid red');
+                        $('#userPass2').css('border', '1.5px solid red');
+                    }else{
+                        $('#userPass1').css('border', '1px solid black');
+                        $('#userPass2').css('border', '1px solid black');
+                    }
+                //Si la contraseña no cumple los requisítos mínimos, detenemos el submit
+                }else{
+                    event.preventDefault();
+                    $('#errorMsgUser > span').text('La contraseña debe incluir 8 caracteres con mayúsculas, minúsculas, números y caracteres especiales');
+                    $('#userPass1').css('border', '1.5px solid red');
+                    $('#userPass2').css('border', '1.5px solid red');
+                }
+            }
+            
+        //Si hay algún campo vacío, detenemos el submit
+        }else{
+            event.preventDefault();
+            $('#errorMsgUser > span').text('Por favor, rellene todos los campos');
+            if(name == "") $('#userName').css('border', '1.5px solid red'); else $('#userName').css('border', '1px solid black');
+            if(pass1 == "") $('#userPass1').css('border', '1.5px solid red'); else $('#userPass1').css('border', '1px solid black');
+            if(pass2 == "") $('#userPass2').css('border', '1.5px solid red'); else $('#userPass2').css('border', '1px solid black');
+        }
+    });
+});
--- resources/views/install.blade.php
+++ resources/views/install.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.access')
 
 @section('headExtension')
-
+    <script type="text/javascript" src="{{ url('js/install/install.js') }}"></script>
 @endsection
 
 @section('content')
 <div id="content" class="col100 centerV row100">
 
-    <div class="col100" style="margin-top:-5%">
+    <div class="col100" style="margin-top:-2%">
         <div class="col100 centerH">
             <img class="col25" src="{{ url('/img/logo.png')}}"/>
         </div>
 
-        <form class="col80 xxlMarginTop" action="{{ route('install.instalation') }}" method="get" style="margin-left: 18%">
+        <form id="installForm" class="col80 xxlMarginTop" action="{{ route('install.instalation') }}" method="get" style="margin-left: 18%">
             <div class="col40" style="margin-right: 13%">
                 <div class="col100"><span class="semiTitle">Para la instalación será necesario introducir el nombre de una base de datos previamente creada</span></div>
                 <div class="col100 mMarginTop sMarginBottom"><label  for="">Nombre de la base de datos</label></div>
-                <div class="co100 sMarginTop"><input type="text" name="BDName" required></div>
+                <div class="co100 sMarginTop"><input class="col80" type="text" name="BDName"></div>
                 <div class="col100 mMarginTop sMarginBottom"><label >Nombre del servidor (localhost por defecto)</label></div>
-                <div class="col100 sMarginTop"><input type="text" name="SName" value="localhost" required></div>
+                <div class="col100 sMarginTop"><input class="col80" type="text" name="SName" value="localhost"></div>
                 <div class="col100 mMarginTop sMarginBottom"><label  for="">Nombre del usuario de la base de datos (root por defecto)</label></div>
-                <div class="col100 sMarginTop"><input type="text" name="UName" value="root" required></div>
+                <div class="col100 sMarginTop"><input class="col80" type="text" name="UName" value="root"></div>
                 <div class="col100 mMarginTop sMarginBottom"><label  for="">Contraseña (sin contraseña por defecto)</label></div>
-                <div class="col100 sMarginTop"><input type="text" name="PName" value=""></div>
+                <div class="col100 sMarginTop"><input class="col80" type="password" name="PName" value=""></div>
                 <div class="col100 mMarginTop sMarginBottom"><label  for="">Sitema operativo actual</label></div>
                 <div class="col100">
-                    <input type="radio" name="Sys" value="windows">
+                    <input type="radio" name="Sys" value="windows"> 
                     <label for="windows">windows</label><br>
                 </div>
                 <div class="col100">
@@ -36,9 +36,14 @@
             <div class="col30">
                 <div class="col100"><span class="semiTitle">Creación de usuario administrador:</span></div>
                 <div class="col100 mMarginTop sMarginBottom"><label  for="name">Nombre</label></div>
-                <div class="col100 sMarginTop"><input type="text" name="Name" value="" required></div>
+                <div class="col100 sMarginTop"><input class="col100" type="text" id="userName" name="Name" value=""></div>
                 <div class="col100 mMarginTop sMarginBottom"><label  for="password">Contraseña  </label></div>
-                <div class="col100 sMarginTop"><input type="text" name="Pass" value="" required></div>
+                <div class="col100 sMarginTop"><input class="col100" type="password" id="userPass1" name="Pass" value=""></div>
+                <div class="col100 mMarginTop sMarginBottom"><label  for="password">Confirma la contraseña  </label></div>
+                <div class="col100 sMarginTop"><input class="col100" type="password" id="userPass2" name="PassC" value=""></div>
+                <div id="errorMsgUser" class="col100">
+                    <span></span>
+                </div>
             </div>
             <div class="col100 lMarginTop" style="margin-left: 25%">
                 <input class="col30" type="submit" value="Crear">
