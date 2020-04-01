2beb75ff8fde64298c22a07e16caff04739576f0
Jose-bousfanj-95 <jose.bousfanj.m@gmail.com>
2020-04-01 10:11:34 +0200

    duplicada variable texto, arreglado.

--- app/Http/Controllers/Install.php
+++ app/Http/Controllers/Install.php
@@ -75,54 +75,6 @@ class Install extends Controller
   
   MIX_PUSHER_APP_KEY=\${PUSHER_APP_KEY}
   MIX_PUSHER_APP_CLUSTER=\${PUSHER_APP_CLUSTER}
-  
-$texto = <<<_END
-APP_NAME=Laravel
-APP_ENV=local
-APP_KEY=base64:LmnXuCC2k6B1E4Rc1s0vCoYW26/8DzeUTtQRVZNTsbo=
-APP_DEBUG=true
-APP_URL=http://$servidor
-
-LOG_CHANNEL=stack
-
-DB_CONNECTION=mysql
-DB_HOST=127.0.0.1
-DB_PORT=3306
-DB_DATABASE=$baseDeDatos
-DB_USERNAME=$usuarioDB
-DB_PASSWORD=$contrasenaDB
-
-BROADCAST_DRIVER=log
-CACHE_DRIVER=file
-QUEUE_CONNECTION=sync
-SESSION_DRIVER=file
-SESSION_LIFETIME=120
-
-REDIS_HOST=127.0.0.1
-REDIS_PASSWORD=null
-REDIS_PORT=6379
-
-MAIL_DRIVER=smtp
-MAIL_HOST=smtp.mailtrap.io
-MAIL_PORT=2525
-MAIL_USERNAME=null
-MAIL_PASSWORD=null
-MAIL_ENCRYPTION=null
-
-AWS_ACCESS_KEY_ID=
-AWS_SECRET_ACCESS_KEY=
-AWS_DEFAULT_REGION=us-east-1
-AWS_BUCKET=
-
-PUSHER_APP_ID=
-PUSHER_APP_KEY=
-PUSHER_APP_SECRET=
-PUSHER_APP_CLUSTER=mt1
-
-MIX_PUSHER_APP_KEY=\${PUSHER_APP_KEY}
-MIX_PUSHER_APP_CLUSTER=\${PUSHER_APP_CLUSTER}
-
-SYSTEM_HOST=$sistema
 _END;
   
     fwrite($fh, $texto) or die("El fichero no se ha podido crear con éxito, realice una copia de este contenido en su fichero .env: <br> <br>
