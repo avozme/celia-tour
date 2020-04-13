a92a4074a99ffe9aab7d18d11957f77ae9cbf20e
Angel Salas Calvo <angelsalascalvo@gmail.com>
2020-04-13 11:35:56 +0200

    Creadas migraciones tablas keys + ranking | issue #107

---
+++ database/migrations/2020_04_13_092349_create_ranking_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRankingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ranking', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nick');
+            $table->integer('time');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ranking');
+    }
+}
