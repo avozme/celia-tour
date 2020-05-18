7d395193ddd8f602a8bb8bcaa9cac1cade0d7279
mmarbonillo <mbonillo95@gmail.com>
2020-05-18 12:36:00 +0200

    Intento de migraciones nuevas

--- database/migrations/2020_05_18_082451_create_escaperooms_table.php
+++ database/migrations/2020_05_18_082451_create_escaperooms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionsTable extends Migration
+class CreateEscaperoomsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('escape_rooms', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('text');
-            $table->string('answer');
-            $table->boolean('key');
-            $table->bigInteger('id_hide')->nullable();
-            $table->bigInteger('id_audio')->nullable();
+            $table->string('name');
+            $table->text('description');
+            $table->smallInteger('difficulty')->default(1);
             $table->dateTime('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrent();
         });
@@ -32,6 +30,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('escaperooms');
     }
 }
---
+++ database/migrations/2020_05_18_092944_add_id_escaperoom_to_questions.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdEscaperoomToQuestion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('questions', function (Blueprint $table) {
+            $table->bigInteger('id_escaperoom');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
