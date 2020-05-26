32846771807e5ca644b02ffd323599477b2678fc
mmarbonillo <mbonillo95@gmail.com>
2020-05-26 12:18:13 +0200

    Modificaciones css para ajustar el textarea dentro de la modal de nueva pista

---
+++ database/migrations/2020_05_26_085754_add_type_to_questions_and_clues.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeToQuestionsAndClues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->smallInteger('type')->default(0);
+        });
+        Schema::table('clues', function (Blueprint $table) {
+            $table->smallInteger('type')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
