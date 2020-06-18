b26f393baa157e2c68e80e7ae3ea93098c0234a0
mmarbonillo <mbonillo95@gmail.com>
2020-06-18 12:43:00 +0200

    Solucionado problema con mapas grandes verticales en la modal de opciones

--- resources/views/admin/options.blade.php
+++ resources/views/admin/options.blade.php
@@ -37,15 +37,15 @@
 @endsection
 @section("modal")
 <!-- MODAL MAPA -->
-<div  id="modalMap" class="window sizeWindow70" style="display: none;">
+<div  id="modalMap" class="window sizeWindow70" style="display: none; max-height: 96%">
         <span class="titleModal col100">SELECCIONAR ESCENA</span>
         <button class="closeModal">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
             <polygon points="28,22.398 19.594,14 28,5.602 22.398,0 14,8.402 5.598,0 0,5.602 8.398,14 0,22.398 5.598,28 14,19.598 22.398,28"/>
         </svg>
         </button>
-        <div class="content col90 mMarginTop">
-            <div id="map1" class="oneMap">
+        <div class="content col90 mMarginTop" style="overflow: auto; max-height: 523px">
+            <div id="map1" class="oneMap col100">
                 @include('backend.zone.map.zonemap')
             </div>
         </div>
